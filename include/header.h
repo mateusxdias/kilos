c394bbc8c3e9f35aa8679a1d77ebc0d6b11c2b76
Mateus Dias <mateusxdias@gmail.com>
2019-05-28 14:27:22 -0300

    funcionando

--- include/header.h
+++ include/header.h
@@ -1,16 +1,16 @@
-#include "soc/rtc.h"
+//Bibliotecas Externas
 #include <ConnectionClass.h>
 #include <ArduinoJson.h>
-#include "esp32-hal-cpu.h"
-#include "HX711.h"
-
 #include <LogClass.h>
-
 #include <EEPROM.h>
+#include <HX711.h>
+
+//Bibliotecas Internas
+#include "esp32-hal-cpu.h"
+#include "soc/rtc.h"
 #include "time.h"
 
-
-//Define configurations
+//Define configurations do MQTT
 const char *SSID = "Positiva";
 const char *PASS = "#Positiva2019#";
 const char *TOPIC_SUBSCRIBE = "kilos";
@@ -18,25 +18,38 @@ const char *TOPIC_PUBLISH = "kilos/value";
 const char *BROKER_MQTT = "mqtt.positiva.app";
 uint16_t BROKER_PORT = 1883;
 
+//Define configurations do Timer
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = 0;
-
-//nao esqueca de ajustar o fuso
 const int daylightOffset_sec = -3600 * 3;
 
+//Define Classes
 ConnectionClass Connection;
 LogClass Log;
+HX711 scale(26, 25);
 
+//Define variáveis
 float value = 0;
-
 long last_msg = 0;
-
-void publish(String _payload1, String _var1, const char *_TOPIC_PUBLISH);
-void hx_setup();
-
 long t;
 
+//Define funções
+void publish(String _payload1, String _var1, const char *_TOPIC_PUBLISH);
+void hx_setup();
 void calibrate();
 void printLocalTime();
-void publish(String _payload1, String _var1, String _payload2, int _var2, const char *_TOPIC_PUBLISH);
-HX711 scale(26, 25);
+
+/*
+LOGS MQTT ->
+
+#define MQTT_CONNECTION_TIMEOUT     -4
+#define MQTT_CONNECTION_LOST        -3
+#define MQTT_CONNECT_FAILED         -2
+#define MQTT_DISCONNECTED           -1
+#define MQTT_CONNECTED               0
+#define MQTT_CONNECT_BAD_PROTOCOL    1
+#define MQTT_CONNECT_BAD_CLIENT_ID   2
+#define MQTT_CONNECT_UNAVAILABLE     3
+#define MQTT_CONNECT_BAD_CREDENTIALS 4
+#define MQTT_CONNECT_UNAUTHORIZED    5
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,8 @@ void setup()
   printLocalTime();
 
   Connection.set_topic(TOPIC_SUBSCRIBE);
-
   Connection.func_mac();
-
   Connection.ota();
-
   Connection.set_server(BROKER_PORT, BROKER_MQTT);
 }
 void loop()
@@ -63,7 +60,7 @@ void loop()
   }
 
   //The magical happens here
-  if ((millis() - last_msg > 10000) && (Connection.mqtt_Connected()))
+  if ((millis() - last_msg > 5000) && (Connection.mqtt_Connected()))
   {
     last_msg = millis();
 
@@ -77,35 +74,8 @@ void loop()
 }
 void hx_setup()
 {
-  // Serial.println("");
-  // Serial.print("read: \t\t");
-  // Serial.println(scale.read()); // print a raw reading from the ADC
-
-  // Serial.print("read average: \t\t");
-  // Serial.println(scale.read_average(20)); // print the average of 20 readings from the ADC
-
-  // Serial.print("get value: \t\t");
-  // Serial.println(scale.get_value(5)); // print the average of 5 readings from the ADC minus the tare weight (not set yet)
-
-  // Serial.print("get units: \t\t");
-  // Serial.println(scale.get_units(5), 1); // print the average of 5 readings from the ADC minus tare weight (not set) divided
-  // // by the SCALE parameter (not set yet)
-
-  scale.set_scale(-104.80); // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.tare();             // reset the scale to 0
-
-  // Serial.print("read: \t\t");
-  // Serial.println(scale.read()); // print a raw reading from the ADC
-
-  // Serial.print("read average: \t\t");
-  // Serial.println(scale.read_average(20)); // print the average of 20 readings from the ADC
-
-  // Serial.print("get value: \t\t");
-  // Serial.println(scale.get_value(5)); // print the average of 5 readings from the ADC minus the tare weight, set with tare()
-
-  // Serial.print("get units: \t\t");
-  // Serial.println(scale.get_units(5), 1); // print the average of 5 readings from the ADC minus tare weight, divided
-  // // by the SCALE parameter set with set_scale
+  scale.set_scale(-104.80); 
+  scale.tare(); 
 }
 void calibrate()
 {
